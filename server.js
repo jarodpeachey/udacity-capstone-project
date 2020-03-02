f07229cdd4b364ea98b70bf362884c0f413dca69
Jarod Peachey <jwpeachey107@aol.com>
2020-03-02 10:54:34 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+// Dependencies
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Empty data object
+projectData = {
+  entries: [],
+};
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware, as well as cors
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('src'));
+
+// Initialize server
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+
+// Set up HTTP request routes
+app.get('/data', sendData);
+app.post('/add', postData);
+
+// Set up functions for HTTP requests
+function sendData(request, response) {
+  response.send(projectData);
+}
+
+function postData(request, response) {
+  newEntry = {
+    date: request.body.date,
+    zip: request.body.zip,
+    feeling: request.body.feelings,
+    temp: ((request.body.temp - 273.15) * (9 / 5) + 32).toFixed(), // Convert to F°
+    name: request.body.name,
+    id:
+      projectData.entries.length > 0
+        ? projectData.entries[projectData.entries.length - 1].id + 1
+        : 1,
+  };
+
+  projectData = {
+    entries: [
+      ...projectData.entries,
+      newEntry,
+    ],
+  };
+
+  response.send(projectData);
+}
---
+++ src/app.js
@@ -0,0 +1,128 @@
+/* Global Variables */
+let addEntryButton = document.getElementById('entry-button');
+let nameElement = document.getElementById('name');
+let zipElement = document.getElementById('zip');
+let feelingsElement = document.getElementById('feelings');
+let entrySection = document.getElementById('entry-section');
+let entriesElement = document.getElementById('entries');
+
+// API Key
+const apiKey = '8e5ccdb71fc19dc417aad096b7f8d68c';
+
+// Request URL
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Event Listener
+addEntryButton.addEventListener('click', addEntry);
+
+// Add Entry
+async function addEntry(e) {
+  e.preventDefault();
+  if (
+    name.value == '' ||
+    zipElement.value == '' ||
+    feelingsElement.value == ''
+  ) {
+    alert('Please fill in all the fields.');
+  } else {
+    getWeatherData(zipElement.value, apiKey);
+  }
+}
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear();
+
+async function getWeatherData(zipCode, apiKey) {
+  const response = await fetch(`${weatherURL}?zip=${zipCode}&APPID=${apiKey}`);
+
+  try {
+    const allData = await response.json();
+
+    if (allData.cod == '404') {
+      alert(allData.message);
+    } else {
+      const newData = {
+        date: newDate,
+        temp: allData.main.temp,
+        zip: zipElement.value,
+        feelings: feelingsElement.value,
+        name: nameElement.value,
+      };
+
+      const postResult = await postData('/add', newData);
+
+      const getResult = await getData('/data');
+
+      displayResults(getResult);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function postData(url, data) {
+  let result = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  let responseData = await result.json();
+
+  return responseData;
+}
+
+async function getData(url) {
+  let result = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  let responseData = await result.json();
+
+  return responseData;
+}
+
+function displayResults(data) {
+  const entries = data.entries;
+  const { name, date, feeling, zip, temp } = entries[entries.length - 1];
+
+  const wrapper = document.createElement('div');
+  const flex = document.createElement('div');
+  const textWrapper = document.createElement('div');
+  const nameElement = document.createElement('em');
+  const dateElement = document.createElement('span');
+  const tempElement = document.createElement('div');
+  const tempIndicator = document.createElement('span');
+  const feelingsElement = document.createElement('p');
+
+  wrapper.classList.add('entry-card');
+  flex.classList.add('entry-flex');
+  nameElement.classList.add('entry-name');
+  dateElement.classList.add('entry-date');
+  tempElement.classList.add('entry-temp');
+  tempIndicator.classList.add('entry-temp-indicator');
+  textWrapper.classList.add('entry-feelings');
+
+  nameElement.innerText = `- ${name}`;
+  dateElement.innerText = date;
+  feelingsElement.innerText = feeling;
+  tempIndicator.innerText = '(F)';
+  tempElement.innerText = `${temp}°`;
+  tempElement.appendChild(tempIndicator);
+
+  flex.appendChild(tempElement);
+  flex.appendChild(dateElement);
+  textWrapper.appendChild(feelingsElement);
+  textWrapper.appendChild(nameElement);
+  wrapper.appendChild(flex);
+  wrapper.appendChild(textWrapper);
+
+  entriesElement.appendChild(wrapper);
+  entrySection.style.display = 'block';
+}
